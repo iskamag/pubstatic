faf2f7973c624a4f1cb5a3d4bab941bcb0ee2c89
Iska Mag <iska@iskamag.com>
2026-05-30 15:44:56 +0500

    concat by default

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd $(dirname $0)
-sudo -u pubstatic npm start > /var/log/pubstatic.log 2>/var/log/pubstatic.err
+sudo -u pubstatic npm start >> /var/log/pubstatic.log 2>>/var/log/pubstatic.err
